f277050c905bc46fd4f436cc9de44a16793ddb44
jayson3110 <jayson7498@gmail.com>
2021-08-10 21:17:55 +0700

    add blog

---
+++ blog/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from . import schemas
+
+app = FastAPI()
+
+@app.post('/blog')
+
+
+
+
+
+
+def create(request: schemas.Blog):
+
+    return  request
+
+
+
--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/blog')
 
-def index():
-    return {'data':'blog list'}
+def index(limit=10, published: bool = True, sort:Optional[str] = None):
+
+    if published:
+        return {'data':f' {limit} published blogs list from the db '}
+
+    else:
+        return {'data':f' {limit} blogs list from the db '}
 
 
 @app.get('/blog/unpublished')
@@ -28,5 +35,20 @@ def show(id: int):
 
 @app.get('/blog/{id}/comments')
 
-def comments(id):
+def comments(id, limit=10):
+
     return {'data': {'1', '2'}}
+
+
+
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published_at: Optional[bool]
+
+
+@app.post('/blog')
+
+def create_blog(request: Blog):
+    return {"data": f"Blog is created with title as {request}"}
